fb245463502de657280abb6af8299f5c754026ce
Diogo Franco <diogo.franco85@gmail.com>
2021-12-13 18:50:20 -0300

    correção de conflitos

--- pages/home/script.js
+++ pages/home/script.js
@@ -2,24 +2,15 @@ import { mapGetters } from 'vuex';
 
 export default {
   data: () => ({
-<<<<<<< HEAD
-    selectClient: [],
-    selectFarm: [],
-=======
     loading: true,
->>>>>>> origin/master
   }),
   middleware: ['auth'],
 
   computed: {
     ...mapGetters({
-<<<<<<< HEAD
-      client: 'Client/getData'
-=======
       average: 'Chart/getAverage',
       hoursDay: 'Chart/getHoursDay',
       cubicMeterDay: 'Chart/getCubicMeterDay',
->>>>>>> origin/master
     })
   },
 
@@ -27,26 +18,9 @@ export default {
     this.loadData();
   },
 
-<<<<<<< HEAD
-  watch: {
-    client(value) {
-      if (value !== []) {
-        value.map(item => {
-          this.selectClient.push({ text: item.name, value: item.id })
-        })
-      }
-    }
-  },
-
-  methods: {
-    loadData() {
-      this.$store.dispatch('Client/GET_LIST');
-    }
-=======
   methods: {
     loadData() {
       this.$store.dispatch('Chart/GET_SHEETS');
     },
->>>>>>> origin/master
   }
 }
